9005a12554c179386930721770e4668ded2d2b39
Kira <kirazy@live.com>
2026-06-07 03:47:33 -0700

    refactor: rework pipe handling to use assets, choice of materials

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,1 +1,1 @@
-require("entity.pipes")
+require("prototypes.entities.pipes")
---
+++ prototypes/entities/pipes.lua
@@ -0,0 +1,169 @@
+local _lib = require("_lib")
+local _framework = { tiers = require("__reskins-framework__.api.tiers") }
+local _assets = {
+	defines = require("__reskins-assets-api__.api.defines"),
+	icons = require("__reskins-assets-api__.api.icons"),
+	create_icon = require("__reskins-assets-api__.assets.base.base-icons"),
+}
+
+local pipe_material_tints = {
+	[_assets.defines.pipe_material.aluminum] = util.color("#ffffff"),
+	[_assets.defines.pipe_material.copper] = util.color("#d45539"),
+	[_assets.defines.pipe_material.stone] = util.color("#cfcfcf"),
+	[_assets.defines.pipe_material.bronze] = util.color("#b09954"),
+	[_assets.defines.pipe_material.steel] = util.color("#877c76"),
+	[_assets.defines.pipe_material.plastic] = util.color("#0078ff"),
+	[_assets.defines.pipe_material.brass] = util.color("#f9c854"),
+	[_assets.defines.pipe_material.titanium] = util.color("#adadb2"),
+	[_assets.defines.pipe_material.ceramic] = util.color("#8f7967"),
+	[_assets.defines.pipe_material.tungsten] = util.color("#3b3b3b"),
+	[_assets.defines.pipe_material.nitinol] = util.color("#706f6b"),
+	[_assets.defines.pipe_material.copper_tungsten] = util.color("#99593d"),
+	[_assets.defines.pipe_material.titanium_angels] = util.color("#995f92"),
+	[_assets.defines.pipe_material.ceramic_angels] = util.color("#ffffff"),
+	[_assets.defines.pipe_material.tungsten_angels] = util.color("#7e5f45"),
+	[_assets.defines.pipe_material.nitinol_angels] = util.color("#7664a9"),
+}
+
+local PipeGraphicsPack = require("__reskins-assets-api__.graphics-packs.base.pipe-graphics-pack")
+local PipeToGroundGraphicsPack = require("__reskins-assets-api__.graphics-packs.base.pipe-to-ground-graphics-pack")
+
+local pipe_entities = {
+	["pipe"] = {
+		type = "pipe",
+		tier = 1,
+		prog_tier = 1,
+		pipe_material = _assets.defines.pipe_material.iron,
+	},
+	["nullius-pipe-2"] = {
+		type = "pipe",
+		tier = 2,
+		prog_tier = 2,
+		pipe_material = _assets.defines.pipe_material.aluminum,
+	},
+	["nullius-pipe-3"] = {
+		type = "pipe",
+		tier = 3,
+		prog_tier = 3,
+		pipe_material = _assets.defines.pipe_material.plastic,
+	},
+	["nullius-pipe-4"] = {
+		type = "pipe",
+		tier = 4,
+		prog_tier = 4,
+		pipe_material = _assets.defines.pipe_material.tungsten,
+	},
+}
+
+for name, options in pairs(pipe_entities) do
+	local entity = data.raw[options.type][name]
+	if not entity then
+		goto continue
+	end
+
+	_lib.create_explosions_and_particles(name, {
+		base_entity_name = "pipe",
+		type = "pipe",
+		tint = pipe_material_tints[options.pipe_material],
+		particles = {
+			["medium"] = 1,
+			["small"] = 2,
+		},
+	})
+
+	local corpse = _lib.create_remnant(name, {
+		base_entity_name = "pipe",
+		type = "pipe",
+	})
+
+	local tier = _framework.tiers.get_tier(options)
+
+	local graphics_pack = PipeGraphicsPack:configure({
+		pipe_material = options.pipe_material,
+	})
+
+	graphics_pack:apply_to_entity(entity)
+	graphics_pack:apply_to_corpse(corpse)
+
+	local icon = _assets.create_icon.pipe(options.pipe_material)
+	---@type DeferrableIconData
+	local deferrable_icon = {
+		name = name,
+		type_name = options.type,
+		icon_data = _framework.tiers.add_tier_labels_to_icons(tier, icon),
+	}
+
+	_assets.icons.assign_deferrable_icon(deferrable_icon)
+
+	::continue::
+end
+
+local pipe_to_ground_entities = {
+	["pipe-to-ground"] = {
+		type = "pipe-to-ground",
+		tier = 1,
+		prog_tier = 1,
+		pipe_material = _assets.defines.pipe_material.iron,
+	},
+	["nullius-underground-pipe-2"] = {
+		type = "pipe-to-ground",
+		tier = 2,
+		prog_tier = 2,
+		pipe_material = _assets.defines.pipe_material.aluminum,
+	},
+	["nullius-underground-pipe-3"] = {
+		type = "pipe-to-ground",
+		tier = 3,
+		prog_tier = 3,
+		pipe_material = _assets.defines.pipe_material.plastic,
+	},
+	["nullius-underground-pipe-4"] = {
+		type = "pipe-to-ground",
+		tier = 4,
+		prog_tier = 4,
+		pipe_material = _assets.defines.pipe_material.nitinol,
+	},
+}
+
+for name, options in pairs(pipe_to_ground_entities) do
+	local entity = data.raw[options.type][name]
+	if not entity then
+		goto continue
+	end
+
+	_lib.create_explosions_and_particles(name, {
+		base_entity_name = "pipe-to-ground",
+		type = "pipe-to-ground",
+		tint = pipe_material_tints[options.pipe_material],
+		particles = {
+			["medium"] = 1,
+			["small"] = 2,
+		},
+	})
+
+	local corpse = _lib.create_remnant(name, {
+		base_entity_name = "pipe-to-ground",
+		type = "pipe-to-ground",
+	})
+
+	local tier = _framework.tiers.get_tier(options)
+
+	local graphics_pack = PipeToGroundGraphicsPack:configure({
+		pipe_material = options.pipe_material,
+	})
+
+	graphics_pack:apply_to_entity(entity)
+	graphics_pack:apply_to_corpse(corpse)
+
+	local icon = _assets.create_icon.pipe_to_ground(options.pipe_material)
+	---@type DeferrableIconData
+	local deferrable_icon = {
+		name = name,
+		type_name = options.type,
+		icon_data = _framework.tiers.add_tier_labels_to_icons(tier, icon),
+	}
+
+	_assets.icons.assign_deferrable_icon(deferrable_icon)
+
+	::continue::
+end
